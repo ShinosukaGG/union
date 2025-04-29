1aa107764ca7d8d5bf7bc78219a22fd0ed339c24
aeryz <abdullaheryz@protonmail.com>
2025-04-29 12:58:05 +0300

    chore: moar bera
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- voyager/plugins/client-update/berachain/src/call.rs
+++ voyager/plugins/client-update/berachain/src/call.rs
@@ -1,8 +1,6 @@
 use enumorph::Enumorph;
-use ibc_union_spec::ClientId;
 use macros::model;
 use unionlabs::ibc::core::client::height::Height;
-use voyager_message::primitives::ChainId;
 
 #[model]
 #[derive(Enumorph)]
@@ -12,8 +10,6 @@ pub enum ModuleCall {
 
 #[model]
 pub struct FetchUpdate {
-    pub counterparty_chain_id: ChainId,
     pub update_from: Height,
     pub update_to: Height,
-    pub client_id: ClientId,
 }
